d2efef7be1cf49204abd876d635eb86bdabe0c92
Liu Xiang <liuxiangxyd@163.com>
2018-06-12 13:47:10 +0800

    add Help Button
    1. Add Help Button，and show the help information.
    2. Add errinfo procedure, and try ... except ValueError as ve, to show error info when entry is not number.

--- szbdt.py
+++ szbdt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created by 刘翔
-Emailto   : liu.xiang@vip.163.com
-Date : 8 June 2018
+Created by ： 刘翔
+Email to   ： liu.xiang@vip.163.com
+Started Date：30 May 2018
 
 一、镀锌、喷涂线槽计算公式
 1. 槽身成本=[2*(高+折边)+宽]*长*槽厚*槽吨价*密度/1,000,000
@@ -11,13 +11,27 @@ Date : 8 June 2018
 3. 喷涂费 = (高+宽)*4*长*每平米喷涂费/1,000
 4. 镀锌每条毛利率=（1-每条成本价/每条销售价）*100%
 5. 喷涂每条毛利率=（1-每条喷涂成本价/每条喷涂销售价）*100%
+6-11: 上述两个公式修改一下，有可能改变运行误差。
+4. 镀锌每条毛利率%=（每条销售价-每条成本价）*100/每条销售价
+5. 喷涂每条毛利率%=（每条喷涂销售价-每条喷涂成本价）*100/每条喷涂销售价
 注意：取小数点后2位。
 
-常用数据：
+常用数据，节省输入时间，方便用户选择，避免错误数据输入：
 长: [2],宽:[60,80,100,150,200],高:[40,60,80,100],折边:[10,15,20],密度:[7.85],
 吨价（槽,盖）:[5150,5200,5250,5350,5800,6150],厚（槽,盖）:[0.35,0.45,0.6,0.7,0.8,0.9,1.0,1.2],
      每平米喷涂费:[0,5.5,6.5,9,10],
 上浮率:[0,5,6,7,8,9,10,11,12,13,14,15]
+
+二、图形界面采用内部模块 tkinter 设计样式
+
+三、Python源码采用pyInstaller工具进行编译发布
+
+    pyinstaller -F -w -i myicon.ico
+
+    注意：请在32位的Python环境下运行，兼容性比较好，可以在Windows7/10 32bit/64bit OS下运行。
+    64位Python环境下发布的执行文件只能在64位操作系统下运行。
+
+TODO：（1）输入数据格式检查，是否为正确的数字格式？（2）关键数字修改，是否自动计算结果？
 
 """
 
@@ -28,6 +42,8 @@ from tkinter.ttk import Combobox
 from sys import argv
 from os.path import exists
 
+# Global variables 全局变量定义设置
+## 可选输入框中的名称和选项列表，采用dict数据格式保存
 lbl_name = {'长':[2],
             '宽':[60,80,100,150,200],
             '高':[40,60,80,100],
@@ -55,26 +71,32 @@ lbl_name = {'长':[2],
             '毛利率2':[]
             }
 
+## 输入框常用长度设置
 entry_len = 7
+## 计算结果缺省长度设置
 result_len =13
+## 列数常数设置
 ncolumn = 5
+## 显示缺省公司名称设置
 defaultCompany = '深圳市八达通线管桥架有限公司'
+## 可选字体选项，用以区分关键结果显示和其他类型
 defaultFont=[("Times New Roman",14),("Calibri",16),("Arial",18)]
 
+# 关键类定义如下Application
 class Application(Frame):
-    """The Main Application
+    """The Main Application, 初始化关键类，界面显示，公式运行等
     """
     ## 分割线，输入行数，和高度值
     def separator(self,rows,heights):
-        ## 行分隔符，输入行数（位置），高度值（大小）
+        ## 行分隔符，输入行数（分隔符位置），高度值（大小）
         sep = Frame(height=heights, bd=1, relief=GROOVE, colormap="new")
         sep.grid(row=rows, sticky=W+E+N+S)
 
     def column_sep(self, columns, widths):
-        ## TODO : 列分隔符，输入列数（位置），宽度（大小）
+        ## TODO : 列分隔符，输入列数（分隔符位置），宽度（大小）
         colsep = Frame(width=widths, bd=1, relief=SUNKEN)
         colsep.grid(column=columns, sticky=W+E+N+S)
-        
+
     def __init__(self, master=None):
         # 初始化应用程序
         Frame.__init__(self, master)
@@ -149,7 +171,7 @@ class Application(Frame):
         self.createPentu(Frame3)
 
     def createEntry(self, frm):
-        ## 第一行 输入框
+        ## 第一行 输入框选择信息标识，采用tkinter.Combobox
         self._lengthInput = Combobox(frm, width=entry_len)
         self._lengthInput['values'] = lbl_name['长']
         self._lengthInput.current(0)  # set the selected item
@@ -174,8 +196,8 @@ class Application(Frame):
         self._mdInput['values'] = lbl_name['密度']
         self._mdInput.current(0)
         self._mdInput.grid(column=9, row=1)
-        
-        ## 第二行输入框
+
+        ## 第二行输入框选择信息标识，采用tkinter.Combobox
         self._cdjInput = Combobox(frm, width=entry_len)
         self._cdjInput['values'] = lbl_name['槽吨价']
         self._cdjInput.current(0)
@@ -185,7 +207,7 @@ class Application(Frame):
         self._gdjInput['values'] = lbl_name['盖吨价']
         self._gdjInput.current(0)
         self._gdjInput.grid(column=3, row=3)
-        
+
         self._chInput = Combobox(frm, width=entry_len)
         self._chInput['values'] = lbl_name['槽厚']
         self._chInput.current(0)
@@ -198,13 +220,18 @@ class Application(Frame):
 
         self._ptfInput = Combobox(frm, width=entry_len)
         self._ptfInput['values'] = lbl_name['喷涂费']
-        self._ptfInput.current(0)
+        self._ptfInput.current(3)
         self._ptfInput.grid(column=9, row=3)
-        
-        ## 按钮“确认”
+
+        ## 按钮“确认”, 计算每条槽身和槽盖的成本价格
         self.alertButton = Button(frm, text='确定', width=12, fg="red",
                         font=defaultFont[0], command=self.calPrice)
         self.alertButton.grid(row=5, column=8, columnspan=2, sticky=E)
+
+        ## "Help" button
+        self.helpButton = Button(frm, text='帮助', width=12, fg="red",
+                        font=defaultFont[0], command=self.helpinfo)
+        self.helpButton.grid(row=5, column=0, columnspan=2, sticky=W)
 
     def createResult(self,frm):
         ## Frame2 输出镀锌线槽结果栏数据显示区域
@@ -239,12 +266,12 @@ class Application(Frame):
         ## 毛利率输入栏： csratio 槽身，cgratio 槽盖
         self._csrInput = Combobox(frm, width=entry_len+5)
         self._csrInput['values'] = lbl_name['上浮率%']
-        self._csrInput.current(0)
+        self._csrInput.current(6)
         self._csrInput.grid(column=6, row=7)
 
         self._cgrInput = Combobox(frm, width=entry_len+5)
         self._cgrInput['values'] = lbl_name['上浮率%']
-        self._cgrInput.current(0)
+        self._cgrInput.current(6)
         self._cgrInput.grid(column=6, row=9)
 
         ## 按钮“计算镀锌销售价格”
@@ -270,7 +297,7 @@ class Application(Frame):
         ## 4. 喷涂上浮率输入栏
         self._ptrInput = Combobox(frm, width=entry_len)
         self._ptrInput['values'] = lbl_name['上浮率%']
-        self._ptrInput.current(0)
+        self._ptrInput.current(6)
         self._ptrInput.grid(column=6, row=15)
         ## 5. 喷涂销售毛利率%
         self._lblptmlv = Label(frm, bg="white", fg="blue", width=entry_len,
@@ -285,66 +312,88 @@ class Application(Frame):
 
     def calPrice(self):
         # Frame2 中计算镀锌成本价格
-        _length = float(self._lengthInput.get())
-        _width  = float(self._widthInput.get())
-        _height = float(self._heightInput.get())
-        _zb = float(self._zbInput.get())
+        try:
+            _length = float(self._lengthInput.get())
+            _width  = float(self._widthInput.get())
+            _height = float(self._heightInput.get())
+            _zb = float(self._zbInput.get())
 
-        _md = float(self._mdInput.get())
-        _cdj = float(self._cdjInput.get())
-        _gdj = float(self._gdjInput.get())
-        _ch = float(self._chInput.get())
-        _gh = float(self._ghInput.get())
+            _md = float(self._mdInput.get())
+            _cdj = float(self._cdjInput.get())
+            _gdj = float(self._gdjInput.get())
+            _ch = float(self._chInput.get())
+            _gh = float(self._ghInput.get())
 
-        self._csj_f = (2*(_height+_zb)+_width)*_length*_ch*_cdj*_md/1000000
-        self._cgj_f = (_width+2*_zb)*_length*_gh*_gdj*_md/1000000
-        self._hjcb_f = self._csj_f + self._cgj_f   #镀锌合计成本价格
+            self._csj_f = (2*(_height+_zb)+_width)*_length*_ch*_cdj*_md/1000000
+            self._cgj_f = (_width+2*_zb)*_length*_gh*_gdj*_md/1000000
+            self._hjcb_f = self._csj_f + self._cgj_f   #镀锌合计成本价格
 
-        self.csj.set(format(self._csj_f, '.2f'))
-        self.cgj.set(format(self._cgj_f, '.2f'))
-        self.hjcb.set(format(self._hjcb_f, '.2f'))
- 
+            self.csj.set(format(self._csj_f, '.2f'))
+            self.cgj.set(format(self._cgj_f, '.2f'))
+            self.hjcb.set(format(self._hjcb_f, '.2f'))
+        except ValueError as ve:
+            self.errinfo("错误信息", ve)
+
     def salePrice(self):
         # Frame2 中计算镀锌销售价格
         self.calPrice()
-        self._csxsj_f = self._csj_f * (1+float(self._csrInput.get())/100)
-        self._cgxsj_f = self._cgj_f * (1+float(self._cgrInput.get())/100)
-        self._hjxsj_f = self._csxsj_f + self._cgxsj_f   #镀锌合计销售价格
+        try:
+            self._csxsj_f = self._csj_f * (1+float(self._csrInput.get())/100)
+            self._cgxsj_f = self._cgj_f * (1+float(self._cgrInput.get())/100)
+            self._hjxsj_f = self._csxsj_f + self._cgxsj_f   #镀锌合计销售价格
 
-        self._dxmlv_f = (1.0-self._hjcb_f/self._hjxsj_f)*100
+            self._dxmlv_f = (self._hjxsj_f-self._hjcb_f)*100/self._hjxsj_f
 
-        self.csxsj.set(format(self._csxsj_f, '.2f'))
-        self.cgxsj.set(format(self._cgxsj_f, '.2f'))
-        self.hjxsj.set(format(self._hjxsj_f, '.2f'))
-        self.maolv1.set(format(self._dxmlv_f, '.2f'))
+            self.csxsj.set(format(self._csxsj_f, '.2f'))
+            self.cgxsj.set(format(self._cgxsj_f, '.2f'))
+            self.hjxsj.set(format(self._hjxsj_f, '.2f'))
+            self.maolv1.set(format(self._dxmlv_f, '.2f'))
+        except ValueError as ve:
+            self.errinfo("错误信息", ve)
+
 
     def calPentu(self):
         # Frame3 中计算喷涂成本价格
         self.salePrice()
-        _length = float(self._lengthInput.get())
-        _width  = float(self._widthInput.get())
-        _height = float(self._heightInput.get())
-        _ptf = float(self._ptfInput.get())
-        ##1 喷涂加工成本价格，有公式
-        self._ptf_f = (_height+_width)*4*_length*_ptf/1000
-        self.ptf.set(format(self._ptf_f, '.2f'))
-        ##2 每条喷涂线槽成本价格=喷涂成本+镀锌成本
-        self._ptcb_f = self._hjcb_f + self._ptf_f
-        self.ptcb.set(format(self._ptcb_f, '.2f'))
-        ##3 Frame3 中计算每条喷涂线槽销售价格=喷涂线槽成本*（1+上浮率%）
-        self._ptxsj_f = self._ptcb_f * (1+float(self._ptrInput.get())/100)
-        self.ptxsj.set(format(self._ptxsj_f, '.2f'))
-        ##4 喷涂销售毛利率
-        self._ptmlv_f = (1.0-self._ptcb_f/self._ptxsj_f)*100
-        self.maolv2.set(format(self._ptmlv_f, '.2f'))
+        try:
+            _length = float(self._lengthInput.get())
+            _width  = float(self._widthInput.get())
+            _height = float(self._heightInput.get())
+            _ptf = float(self._ptfInput.get())
+            ##1 喷涂加工成本价格，有公式
+            self._ptf_f = (_height+_width)*4*_length*_ptf/1000
+            self.ptf.set(format(self._ptf_f, '.2f'))
+            ##2 每条喷涂线槽成本价格=喷涂成本+镀锌成本
+            self._ptcb_f = self._hjcb_f + self._ptf_f
+            self.ptcb.set(format(self._ptcb_f, '.2f'))
+            ##3 Frame3 中计算每条喷涂线槽销售价格=喷涂线槽成本*（1+上浮率%）
+            self._ptxsj_f = self._ptcb_f * (1+float(self._ptrInput.get())/100)
+            self.ptxsj.set(format(self._ptxsj_f, '.2f'))
+            ##4 喷涂销售毛利率
+            self._ptmlv_f = (self._ptxsj_f-self._ptcb_f)*100/self._ptxsj_f
+            #(1.0-self._ptcb_f/self._ptxsj_f)*100
+            self.maolv2.set(format(self._ptmlv_f, '.2f'))
+            
+        except ValueError as ve:
+            self.errinfo("错误信息", ve)
 
+    def errinfo(self, title, msg):
+        messagebox.showerror(title, msg)
+
+    def helpinfo(self):
+        messagebox.showinfo("帮助信息",
+        "运行方式：\n\
+            1. bdt \n\
+               公司名称显示为缺省的公司名。 \n\
+            2. bdt 公司名称 \n\
+               更改公司名称为  公司名称")
 
 if __name__ == '__main__':
     apptitle = defaultCompany
     if len(argv) > 1:
         _, apptitle = argv
-    if apptitle.lower() in ['-h','--help','-help','-?']:
-        print("""
+    """if apptitle.lower() in ['-h','--help','-help','-?']:
+        print(""
               运行方式：
               1. szbdt
                  公司名称显示为缺省的公司名：深圳市八达通线管桥架有限公司
@@ -352,8 +401,8 @@ if __name__ == '__main__':
                  更改公司名称为  ‘新公司名’
               3. szbdt -help
                  显示本帮助信息。
-              """)
-        apptitle = defaultCompany
+              "")
+        apptitle = defaultCompany"""
 
     root = Tk()
     root.resizable(width=False, height=False)
